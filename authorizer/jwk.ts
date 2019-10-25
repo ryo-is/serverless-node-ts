acfd34c13e95dec9e58f3b154507b93f55c5b6c2
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-10-25 20:31:57 +0900

    mask

--- authorizer/jwk.ts
+++ authorizer/jwk.ts
@@ -1,32 +1,8 @@
 export default {
   alg: "RS256",
   e: "AQAB",
-  kid: "slkaZ7xBcX/EOFS++K9pcUjHcN7vym6YkaNyT6tP7dE=",
+  kid: "xxxxxxxxx",
   kty: "RSA",
-  n:
-    "gi0stBqGzKyRaNFRwX-4MEiXbGsl6OIchNmE_N9Glhfz58Bn07C9pckikfe5xxK6Qlibjz6VETv0hO9T9l0tWC8WYqmWGYtACKbm3L-2pJLOi-CjkXfPJZAQHeU-NMmY-9nHU1LDGgthgYDXPOYBuFVYcvB2SGCAzJiuA6-3mzYWR9X-fZzxDr6YcRPwwH-CwPvp2kAtAus36gLctly7q7WhSYGv_9qkeEhutL_nOiRxbYJooda5VVLog7U7GugC9ahdNj7hwENuvn00yFVXISWh3ZFfY99vWyN13Oq5cMrD9eCDK3PlG13loaihWDrQj3GNBdA8lI6StlipMQosDw",
+  n: "xxxxxxxxxx",
   use: "sig"
 }
-
-// export default {
-//   keys: [
-//     {
-//       alg: "RS256",
-//       e: "AQAB",
-//       kid: "slkaZ7xBcX/EOFS++K9pcUjHcN7vym6YkaNyT6tP7dE=",
-//       kty: "RSA",
-//       n:
-//         "gi0stBqGzKyRaNFRwX-4MEiXbGsl6OIchNmE_N9Glhfz58Bn07C9pckikfe5xxK6Qlibjz6VETv0hO9T9l0tWC8WYqmWGYtACKbm3L-2pJLOi-CjkXfPJZAQHeU-NMmY-9nHU1LDGgthgYDXPOYBuFVYcvB2SGCAzJiuA6-3mzYWR9X-fZzxDr6YcRPwwH-CwPvp2kAtAus36gLctly7q7WhSYGv_9qkeEhutL_nOiRxbYJooda5VVLog7U7GugC9ahdNj7hwENuvn00yFVXISWh3ZFfY99vWyN13Oq5cMrD9eCDK3PlG13loaihWDrQj3GNBdA8lI6StlipMQosDw",
-//       use: "sig"
-//     },
-//     {
-//       alg: "RS256",
-//       e: "AQAB",
-//       kid: "+nIvBdC1Y1QyL74xezL/lSq2yw5txwS3Qzp0WjBnFgM=",
-//       kty: "RSA",
-//       n:
-//         "muljy1w9sLkiSxs8B96iig-soqf8C3T1sHqzxpBcc5XQt8dXiIix4H7nBKCk1rb9TjYpWMmi_xwtvyqqpuBnEaRngsXtY2z-_-e6Xv0TfqLQy0N3ia1xINZ8Gr7D4aIqtE4_3FU_hnM_KWDCB7rIBX5uQlNb_r3ZyHLmeCz5Q-qoE-RiCAd2YyvkgU4mFR_RfgyBOurekZJRZiOTsovE8ZqxSM6ygjMpo7iKkukYKTL_C6HBhtgbhgje54_4elV3vv0DWlMWPDj8NYvOQv70jA60nhF01-b3Ba9-q5KoBhDcvEdmpsppicyNNM857nSeuqGbax3JESGpCYY_WDeHpQ",
-//       use: "sig"
-//     }
-//   ]
-// }
